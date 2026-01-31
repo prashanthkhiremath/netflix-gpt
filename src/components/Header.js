5008c0bfe6577b5cf22664225b15344432b71b76
prashanthkhiremat@gmail.com <prashanthkumar.m@buhlergroup.com>
2026-01-31 10:37:28 +0530

    Multi Language Support in GPT Search Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,15 @@ import { useEffect } from 'react'
 import { onAuthStateChanged } from "firebase/auth";
 import { addUser, removeUser } from "../utils/userSlice"
 import { useDispatch } from 'react-redux';
-import { LOGO } from "../utils/constants";
+import { LOGO, SUPPORTED_LANGUAGES } from "../utils/constants";
+import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 function Header() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector(store => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch)
   const handleSignOut = () => {
     signOut(auth)
     .then(() => {
@@ -37,6 +40,16 @@ function Header() {
 
     return() => unsubscribe();
   }, [])
+
+  const handleGptSearchClick = () => {
+    // Toggle GPT Search
+    dispatch(toggleGptSearchView());
+  }
+
+  const handleLanguageChange =(e) => {
+    dispatch(changeLanguage(e.target.value));
+  }
+
   return (
     <div className='fixed w-screen px-8 py-2 bg-gradient-to-b from-black z-50 flex justify-between items-center'>
       <img 
@@ -46,31 +59,43 @@ function Header() {
       />   
 
       {user && (
-        <div className='flex items-center group relative p-2'>
-          {/* User Avatar Section */}
-          <div className="flex items-center cursor-pointer">
-            <img
-              className='w-10 h-10 rounded-md' 
-              alt="usericon"
-              src={user?.photoURL} 
-            />
-            <span className="text-white ml-2 transition-transform group-hover:rotate-180 duration-300 text-[10px]">
-              ▼
-            </span>
-          </div>
+        <div className='flex items-center'>
+        {showGptSearch && (<select className="p-2 m-2 bg-gray-900 text-white" onChange={handleLanguageChange}>
+            { SUPPORTED_LANGUAGES.map(lang => 
+              <option key={lang.identifier} 
+                value={lang.identifier}>
+                {lang.name}
+              </option>) }
+          </select>)}
+          <button 
+            className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg hover:bg-opacity-80 transition-all font-medium"
+            onClick={handleGptSearchClick}>
+              { showGptSearch ? "Homepage" : "GPT Search" }
+          </button>
 
-          {/* Simplified Dropdown - Shows only Sign Out */}
-          <div className='absolute top-14 right-0 bg-black/90 border border-gray-800 w-36 hidden group-hover:block transition-all shadow-xl rounded-sm'>
-            
-            {/* Invisible bridge to prevent menu from closing when moving mouse */}
-            <div className="absolute -top-4 w-full h-4"></div>
+          {/* 2. User Profile Group (Only this handles the dropdown) */}
+          <div className='group relative p-2 flex items-center'>
+            <div className="flex items-center cursor-pointer">
+              <img
+                className='w-10 h-10 rounded-md' 
+                alt="usericon"
+                src={user?.photoURL} 
+              />
+              <span className="text-white ml-2 transition-transform group-hover:rotate-180 duration-300 text-[10px]">
+                ▼
+              </span>
+            </div>
 
-            <div className="p-4 text-center">
-              <button 
-                onClick={handleSignOut}
-                className='text-white text-sm font-medium hover:underline w-full'>
-                Sign Out
-              </button>
+            {/* Dropdown Menu */}
+            <div className='absolute top-14 right-0 bg-black/90 border border-gray-800 w-36 hidden group-hover:block transition-all shadow-xl rounded-sm'>
+              <div className="absolute -top-4 w-full h-4"></div>
+              <div className="p-4 text-center">
+                <button 
+                  onClick={handleSignOut}
+                  className='text-white text-sm font-medium hover:underline w-full'>
+                  Sign Out
+                </button>
+              </div>
             </div>
           </div>
         </div>
